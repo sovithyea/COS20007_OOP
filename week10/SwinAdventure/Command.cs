1a97c95515b2d5562e1b348ea92a18f195294eda
sovithyea <prachsovithyea11@gmail.com>
2025-05-14 01:50:14 +1000

    finished 10.2

---
+++ week10/SwinAdventure/Command.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+namespace SwinAdventure
+{
+    public abstract class Command : IdentifiableObject
+    {
+        public Command(string[] ids) : base(ids)
+        {
+        }
+        public abstract string Execute(Player p, string[] text);
+    }
+}
